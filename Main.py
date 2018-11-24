b7dee9129d19ade15e0df7b013236c30b8d99b3f
hannenule <hannenule@outlook.com>
2018-11-23 19:26:15 -0500

    Se creo la funcion Main

--- Main.py
+++ Main.py
@@ -1,2 +1,10 @@
-if __name__ == '__main__':
+# coding: utf8
+
+# Procedimiento principal, punto de inicio de la aplicacion
+def Main():
 	print("Hola Mundo")
+
+
+# Inicio de la aplicacion. NoTocar.
+if __name__ == '__main__':
+	Main()
